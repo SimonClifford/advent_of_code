362d7cfc8aa04901106f3289d3f1ef453db92284
Simon Clifford <sjc306@cam.ac.uk>
2023-12-08 14:11:08 +0000

    Annoying answer for day 8 part 2

--- 2023/jl/8/8-2.jl
+++ 2023/jl/8/8-2.jl
@@ -6,23 +6,63 @@ function main()
     lines = readlines(open(ARGS[1]))
     @views directions = lines[1]
     tree_dict = process_lines(lines)
-    current_locs = [l for l in keys(tree_dict) if endswith(l, 'A')]
-    steps = 0
-    for d in Iterators.cycle(directions)
-        # println("$steps $current_locs")
-        if mod(steps, 1000000) == 0
-            println("$steps")
-        end
-        if all(c->endswith(c, 'Z'), current_locs)
-            break
-        end
-        current_locs = map(current_locs) do l
-            move_loc(tree_dict, l, d)
-        end
-        steps += 1
-    end
-    println("Took $steps steps")
+    start_locs = [l for l in keys(tree_dict) if endswith(l, 'A')]
+    # Now for each start_loc we compute the cycle.
+    cycles = map(s->get_cycle(tree_dict, directions, s), start_locs)
+    # For annoying reasons there is a simple answer
+    ans = lcm([c[3] for c in cycles])
+    println("Answer: $ans")
+    # But this makes a host of maybe unwarranted assumptions!
 end
+
+
+function is_within(cycle::Tuple{Int, Int}, index::Int)
+    return mod(index, cycle[2]) == cycle[1]
+end
+
+"""
+Given a starting location `start_loc` we compute the 'cycle' of
+this location.
+
+We start at start_loc and move through the tree noting all
+occurances of winning locations (i.e. those that end with 'Z').
+We do this until we reach somewhere we've been
+while at the same point of the directions list.
+
+Returns
+    Tuple{Vector{Int}, Int} # winning locations, cycle length
+"""
+function get_cycle(
+    tree::Dict{
+               AbstractString,
+               @NamedTuple{left::AbstractString, right::AbstractString}
+              },
+    directions::AbstractString,
+    start_loc::AbstractString,
+    )
+    cur_loc = start_loc
+    winners = Vector{Int}()
+    past_locs = Dict{Tuple{AbstractString, Int}, Int}()
+    past_locs[(cur_loc, 1)] = 1
+    index = 0
+    while true
+        for (d_ind, d) in enumerate(directions)
+#            println("$index: $cur_loc $d($d_ind)")
+            if endswith(cur_loc, 'Z')
+                append!(winners, index)
+            end
+            cur_loc = move_loc(tree, cur_loc, d)
+            if get(past_locs, (cur_loc, d_ind), 0) != 0
+#                println("would have gone to $cur_loc with $d($d_ind)")
+                return (past_locs[(cur_loc, d_ind)], winners, index - past_locs[(cur_loc, d_ind)])
+            end
+            past_locs[(cur_loc, d_ind)] = index
+            index += 1
+        end
+    end
+end
+
+
 
 function move_loc(
     tree:: Dict{
