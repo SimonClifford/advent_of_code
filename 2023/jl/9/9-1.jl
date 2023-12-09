2c673cd6dd32136d00aa3fdad32ffef1c37bed78
Simon Clifford <sjc306@cam.ac.uk>
2023-12-09 09:48:42 +0000

    Day 9 julia

---
+++ 2023/jl/9/9-1.jl
@@ -0,0 +1,30 @@
+function parse_file(filename::AbstractString)
+    f = open(filename)
+    inputs = map(split.(readlines(f))) do c parse.(Int, c) end
+    return inputs
+end
+
+function main()
+    inputs::Vector{Vector{Int}} = parse_file(ARGS[1])
+    ans = sum(get_next.(inputs))
+    println("Answer: $ans")
+end
+
+"""
+A recursive solution to the problem.
+
+Given a Vector of Ints find the next element by
+recursively finding the next element in the sequences of
+the differences.
+"""
+function get_next(seq::Vector{Int})
+    differences = diff(seq)
+    if all(e->e==0, differences)
+        return seq[end]
+    end
+    return seq[end] + get_next(differences)
+end
+
+if ! isinteractive()
+    main()
+end
